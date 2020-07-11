8b9da4829b7341240bf9891a979eb49f65ff9666
William AbouSharaf <liams_1998@hotmail.com>
2020-07-11 22:54:25 +0300

    Add select indicator to characters + logic

---
+++ GMTK 2020/gameObjects/SelectIndicator.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.hide();
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+func ShowIndicator():
+	self.show();
+
+func HideIndicator():
+	self.hide();
